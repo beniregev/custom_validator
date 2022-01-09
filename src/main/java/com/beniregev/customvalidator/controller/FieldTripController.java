4801df8111f677bf31916f016d4badbcdb212b4b
binyamin.regev <beniregev@gmail.com>
2022-01-09 09:41:46 +0200

    Adding code in controller class

--- src/main/java/com/beniregev/customvalidator/controller/FieldTripController.java
+++ src/main/java/com/beniregev/customvalidator/controller/FieldTripController.java
@@ -1,9 +1,33 @@
 package com.beniregev.customvalidator.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.beniregev.customvalidator.dto.APIResponse;
+import com.beniregev.customvalidator.dto.FieldTripForm;
+import com.beniregev.customvalidator.service.FieldTripService;
+import com.beniregev.customvalidator.validator.FieldTripFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class FieldTripController {
+    @Autowired
+    private FieldTripFormValidator ftfValidator;
+    @Autowired
+    private FieldTripService ftService;
+    @InitBinder(value = "fieldTripForm")
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(ftfValidator);
+    }
+    @PostMapping(value = "/fieldtrip")
+    public ResponseEntity<APIResponse> signUp(@Valid @RequestBody
+                                                      FieldTripForm ftf){
+        APIResponse apiResponse = new APIResponse();
+        //perform your logic here
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
 }
